90c295f0a06d6e4d27ed051ed92f7b2498d6bb29
Isidoro Martinez <isidoro.martinez@petfre.gi>
2016-09-20 15:55:32 +0200

    Responsiveness photogallery and carousel

--- src/app/shared/article/panel/panel.controller.js
+++ src/app/shared/article/panel/panel.controller.js
@@ -4,18 +4,7 @@
     angular.module('panel.controller', []);
 
     function PanelController($scope) {
-        // $scope.header = "";
-        // $scope.body = "Our templates are updated regularly so they don't breaks.";
-        // $scope.bodystyle = "";
-        // $scope.colclass = "col-lg-3 col-xs-1";
-        // $scope.footer = "";
-        // $scope.footerstyle = "";
-        // $scope.imgalt = "Cinque Terre";
-        // $scope.imgheight = "236";
-        // $scope.imgpath = "";
-        // $scope.imgshape = "img-circle";
-        // $scope.imgwidth = "304";
-        // $scope.panelstyle = "panel-warning";
+        
     }
     angular
         .module('panel.controller')
